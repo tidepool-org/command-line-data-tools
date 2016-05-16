ebf556fc535511597133c47e8bd6725f470de4d7
--add <ethan@lookfamily.org>
2016-05-16 09:39:25 -0700

    Cleaning up to start the day. Don't know why this didn't get pushed on Friday.

--- test/test-stripdata.js
+++ test/test-stripdata.js
@@ -1,8 +1,8 @@
 // This does nothing... yet.
-describe('Blobs', function() {
-  it('should list ALL blobs on /blobs GET');
-  it('should list a SINGLE blob on /blob/<id> GET');
-  it('should add a SINGLE blob on /blobs POST');
-  it('should update a SINGLE blob on /blob/<id> PUT');
-  it('should delete a SINGLE blob on /blob/<id> DELETE');
+describe('stripdata', function() {
+	it('should not modify data when no options');
+});
+
+describe('', function() {
+	it('should ');
 });
