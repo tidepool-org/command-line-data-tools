f319f7977945f91f8c47701b8e54ba8fd385d720
--add <ethan@lookfamily.org>
2016-06-06 10:30:25 -0700

    prep for filterdata testing

---
+++ test/test-filterdata.js
@@ -0,0 +1,16 @@
+var should = require('chai').should();
+var fs = require('fs');
+
+var filterdata = require('../bin/filterdata.js');
+
+const IN_FILENAME = 'test/testdata-filterdata.json';
+const OUT_FILENAME = 'test/testdataout-filterdata.json';
+
+describe('filterdata', function() {
+	
+	describe('general functionality', function() {
+		it('should see a test.');
+	});
+
+	
+});
