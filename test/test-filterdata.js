fa353731ae0e18545a2f6058c58738ce00844e20
--add <ethan@lookfamily.org>
2016-06-06 17:49:39 -0700

    more boilerplate for filterdata tests

--- test/test-filterdata.js
+++ test/test-filterdata.js
@@ -9,8 +9,13 @@ const OUT_FILENAME = 'test/testdataout-filterdata.json';
 describe('filterdata', function() {
 	
 	describe('general functionality', function() {
-		it('should see a test.');
+		it('should not succeed when there is no data.');
+		it('should not succeed for to short of a length of data.');
+		it('should not succeed for data with many long gaps.');
+		it('should not succeed for data with a low amount of events per day.');
+		it('should succeed for an amount of data longer than the minimum amount.');
+		it('should succeed for data that begins with a long gap, but then has sufficient data.');
+		it('should succeed for data that does not have a large gap and is not sparse.');
+		it('should succeed for data that ')
 	});
-
-	
 });
