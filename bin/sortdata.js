129e832e7ec7560c514924417a609ef3f38507ed
Eden Grown-Haeberli <edengh@gmail.com>
2017-07-21 09:56:10 -0700

    changes for data export service

--- bin/sortdata.js
+++ bin/sortdata.js
@@ -1,67 +1,72 @@
 #!/usr/bin/env node --harmony
 
-var program = require('commander');
-var fs = require('fs');
-var chalk = require('chalk');
-var path = require('path');
+exports.sortData = sortData;
 
-program
-	.version('0.0.1')
-	.option('-i, --input <input>', 'path/to/input.json')
-	.option('-o, --output <output>', 'path/to/output.json')
-	.option('-v, --verbose', 'Verbose output.')
-	.parse(process.argv);
-
-sortData(program.input, program.output, function() {
-	process.exit(0);
-});
-
-function sortData(input, output, callback) {
-	if (program.verbose) 
-		printOptions();
-
-	if (program.verbose)
-		console.log(chalk.yellow.bold('Reading in data...'));
-
-	var data = require(path.resolve(process.cwd(), input));
-
-	if (program.verbose)
-		console.log(chalk.yellow.bold('Sorting data...'));
-
+function sortData(data) {
 	data.sort(function(a, b) {
 		return new Date(b.time).getTime() - new Date(a.time).getTime();
 	});
-
-	if (program.verbose)
-		console.log(chalk.yellow.bold('Writing data...'));
-
-	var outfileStream = makeOutfileStream(output);
-	writeToOutfileStream(output, outfileStream, JSON.stringify(data));
-
-	if (program.verbose)
-		console.log(chalk.yellow.bold('Done sorting and writing data...'));
 }
 
-function printOptions() {
-	console.log(chalk.blue.bold('input: ') + program.input);
-	console.log(chalk.blue.bold('output: ') + program.output);
-	console.log(chalk.blue.bold('verbose: ') + program.verbose);
-}
+if (require.main === module) {
+	var program = require('commander');
+	var fs = require('fs');
+	var chalk = require('chalk');
+	var path = require('path');
 
-function makeOutfileStream(output) {
-	var ofs;
-	if (output) {
-		ofs = fs.createWriteStream(output);
-	} else {
-		ofs = process.stdout;
+	program
+		.version('0.0.1')
+		.option('-i, --input <input>', 'path/to/input.json')
+		.option('-o, --output <output>', 'path/to/output.json')
+		.option('-v, --verbose', 'Verbose output.')
+		.parse(process.argv);
+
+		if (program.verbose)
+			printOptions();
+
+		if (program.verbose)
+			console.log(chalk.yellow.bold('Reading in data...'));
+		var data = require(path.resolve(process.cwd(), program.input));
+
+		if (program.verbose)
+			console.log(chalk.yellow.bold('Sorting data...'));
+
+		sortData(data);
+
+		if (program.verbose)
+			console.log(chalk.yellow.bold('Writing data...'));
+
+		var outfileStream = makeOutfileStream(output);
+		writeToOutfileStream(program.output, outfileStream, JSON.stringify(data));
+
+		if (program.verbose)
+			console.log(chalk.yellow.bold('Done sorting and writing data...'));
+
+/*sortData(program.input, program.output, function() {
+	process.exit(0);
+});*/
+
+	function printOptions() {
+		console.log(chalk.blue.bold('input: ') + program.input);
+		console.log(chalk.blue.bold('output: ') + program.output);
+		console.log(chalk.blue.bold('verbose: ') + program.verbose);
 	}
-	return ofs;
-}
 
-function writeToOutfileStream(output, outfileStream, info) {
-	if (output) {
-		outfileStream.write(info);
-	} else {
-		console.log(info);
+	function makeOutfileStream(output) {
+		var ofs;
+		if (output) {
+			ofs = fs.createWriteStream(output);
+		} else {
+			ofs = process.stdout;
+		}
+		return ofs;
+	}
+
+	function writeToOutfileStream(output, outfileStream, info) {
+		if (output) {
+			outfileStream.write(info);
+		} else {
+			console.log(info);
+		}
 	}
 }
--- bin/stripdata.js
+++ bin/stripdata.js
@@ -1,108 +1,26 @@
 #!/usr/bin/env node --harmony
 
-var program = require('commander');
-var fs = require('fs');
-var chalk = require('chalk');
-var JSONStream = require('JSONStream');
 var crypto = require('crypto');
 
 exports.program = program;
 exports.performDataStripping = performDataStripping;
 exports.printOptions = printOptions;
-exports.splitDeviceId = splitDeviceId;
+//exports.splitDeviceId = splitDeviceId;
 exports.hashIDsForData = hashIDsForData;
+exports.removeSourceForData = removeSourceForData;
+exports.removeTransmitterIdForData = removeTransmitterIdForData;
+exports.removeIDsAndPayload = removeIDsAndPayload;
+exports.removeAnnotations = removeAnnotations;
+//exports.stripBasalSuppressedInfo = stripBasalSuppressedInfo;
+exports.stripModelAndSNForData = stripModelAndSNForData;
+exports.stripData = stripData;
 
-program
-	.version('0.0.1')
-	.option('-i, --input <input>', 'path/to/input.json')
-	.option('-o, --output <output>', 'path/to/output.json')
-	.option('--stripModels <stripModels>',
-		'Strip model name for these models. e.g. Anonymous Pump', list, [])
-	.option('--stripSNs <stripSNs>',
-		'Strip serial number for these models.', list, [])
-	.option('--leaveModels <leaveModels>',
-		'Leave model for these models. Takes precedence over strip.', list, [])
-	.option('--leaveSNs <leaveSNs>',
-		'Leave serial number for these models. Takes precedence over strip.', list, [])
-	.option('--stripAll',
-		'Strip all of the data, except for what is explicitly left.')
-	.option('--removeTypes <removeTypes>',
-		'Remove these data types.', list, [])
-	.option('--leaveTypes <leaveTypes>',
-		'Leave these data types. Takes precedence over removal.', list, [])
-	.option('--removeAll',
-		'Remove all data types, except for what is explicitly left.')
-	.option('--hashIDs',
-		'Pass IDs (such as _groupId and uploadId) through a one-way hash.')
-	.option('--removeSource',
-		'Remove the source of the data, e.g. carelink.')
-	.option('--removeTransmitter',
-		'Remove the transmitter id, e.g. the transmitter id for a Dexcom.')
-	.option('-v, --verbose',
-		'Verbose output.')
-	.parse(process.argv);
+function removeSourceForData(data) {
+	delete data.source;
+}
 
-performDataStripping(function() {});
-
-function performDataStripping(callback) {
-	checkOptions();
-	if (program.verbose) {
-		printOptions();
-	}
-
-	var ifs = makeInFileStream();
-
-	var jsonStream = JSONStream.parse('*');
-
-	var ofs = makeOutstream();
-
-	var first = true;
-	writeToOutstream(ofs, '[');
-
-	// Perform the parsing
-	ifs
-		.pipe(jsonStream)
-		.on('data', function (chunk) {
-
-			if ((program.removeAll
-				|| program.removeTypes
-					.indexOf(chunk.type) >= 0)
-				&& program.leaveTypes
-					.indexOf(chunk.type) < 0) {
-				// Do NOT add this event to output
-				return;
-			}
-
-			if (first)
-				first = false;
-			else
-				writeToOutstream(ofs, ',');
-
-			var cleanData = chunk;
-
-			stripBasalSuppressedInfo(cleanData);
-
-			removeAnnotations(cleanData);
-
-			removeIDsAndPayload(cleanData);
-
-			stripModelAndSNForData(cleanData);
-
-			hashIDsForData(cleanData);
-
-			removeSourceForData(cleanData);
-
-			removeTransmitterIdForData(cleanData);
-
-			writeToOutstream(ofs, JSON.stringify(cleanData));
-		})
-		.on('end', function() {
-			writeToOutstream(ofs, ']');
-			if (program.verbose) {
-				console.log(chalk.yellow.bold('Done writing to output.'));
-			}
-			callback();
-		});
+function removeTransmitterIdForData(data) {
+	delete data.transmitterId;
 }
 
 function list(val) {
@@ -128,7 +46,6 @@ function splitDeviceId(deviceId) {
 }
 
 function removeIDsAndPayload(data) {
-	if (program.stripAll) {
 		if (data.payload)
 			delete data.payload;
 		if (data.id)
@@ -137,7 +54,6 @@ function removeIDsAndPayload(data) {
 			delete data.guid;
 		if (data.type == 'wizard' && data.bolus)
 			delete data.bolus;
-	}
 }
 
 function removeAnnotations(data) {
@@ -145,8 +61,7 @@ function removeAnnotations(data) {
 		delete data.annotations;
 }
 
-function stripBasalSuppressedInfo(data) {
-	if (program.stripAll) {
+/*function stripBasalSuppressedInfo(data) {
 
 		dataToStrip = data;
 
@@ -157,55 +72,41 @@ function stripBasalSuppressedInfo(data) {
 			delete dataToStrip.suppressed.annotations;
 
 			dataToStrip = dataToStrip.suppressed;
-		}
 	}
-}
+}*/
 
 function stripModelAndSNForData(data) {
 
 	var deviceId = splitDeviceId(data.deviceId);
 	var deviceComp = deviceId[0];
 
-	if ((program.stripAll
-		|| program.stripModels
-			.indexOf(deviceComp) >= 0)
-		&& program.leaveModels
-			.indexOf(deviceComp) < 0) {
-		deviceId[0]=data.type + ' device';
-		if (data.type === 'upload') {
-			// This probably isn't the best way to
-			// go about scrubbing an upload but I
-			// can't discern a better way to go
-			// about it.
-			delete data.deviceManufacturers;
-			if (data.payload)
-				delete data.payload.devices;
-			if (data.deviceModel)
-				data.deviceModel = data.deviceTags[0]
-										+ ' model';
-			if (data.deviceSerialNumber)
-				data.deviceSerialNumber = 'Serial Number';
-		}
+	deviceComp=data.type + ' device';
+	if (data.type === 'upload') {
+		// This probably isn't the best way to
+		// go about scrubbing an upload but I
+		// can't discern a better way to go
+		// about it.
+		delete data.deviceManufacturers;
+		if (data.payload)
+			delete data.payload.devices;
+		if (data.deviceModel)
+			data.deviceModel = data.deviceTags[0]
+									+ ' model';
+		if (data.deviceSerialNumber)
+			data.deviceSerialNumber = 'Serial Number';
 	}
 
-	if ((program.stripAll
-		|| program.stripSNs
-			.indexOf(deviceComp) >= 0)
-		&& program.leaveSNs
-			.indexOf(deviceComp) < 0) {
-		deviceId[1]='Serial Number';
-	}
+	deviceId[1]='Serial Number';
 
 	data.deviceId = deviceId.join('-');
 }
 
 function hashIDsForData(data) {
-	if (program.hashIDs) {
-		if (data._groupId)
-			data.hash_groupId =
-				crypto.createHash('sha256')
-					.update(data._groupId.toString())
-					.digest('hex');
+	if (data._groupId)
+		data.hash_groupId =
+			crypto.createHash('sha256')
+				.update(data._groupId.toString())
+				.digest('hex');
 		delete data._groupId;
 		if (data.uploadId)
 			data.hash_uploadId =
@@ -219,69 +120,177 @@ function hashIDsForData(data) {
 					.update(data.byUser.toString())
 					.digest('hex');
 		delete data.byUser;
+}
+
+function stripData(callback) {
+
+	stripBasalSuppressedInfo(data);
+
+	removeAnnotations(data);
+
+	removeIDsAndPayload(data);
+
+	stripModelAndSNForData(data);
+
+	hashIDsForData(data);
+
+	removeSourceForData(data);
+
+	removeTransmitterIdForData(data);
+
+	callback();
+}
+
+if (require.main === module) {
+
+	var program = require('commander');
+	var fs = require('fs');
+	var chalk = require('chalk');
+	var JSONStream = require('JSONStream');
+
+	program
+		.version('0.0.1')
+		.option('-i, --input <input>', 'path/to/input.json')
+		.option('-o, --output <output>', 'path/to/output.json')
+		.option('--stripModels <stripModels>',
+			'Strip model name for these models. e.g. Anonymous Pump', list, [])
+		.option('--stripSNs <stripSNs>',
+			'Strip serial number for these models.', list, [])
+		.option('--leaveModels <leaveModels>',
+			'Leave model for these models. Takes precedence over strip.', list, [])
+		.option('--leaveSNs <leaveSNs>',
+			'Leave serial number for these models. Takes precedence over strip.', list, [])
+		.option('--stripAll',
+			'Strip all of the data, except for what is explicitly left.')
+		.option('--removeTypes <removeTypes>',
+			'Remove these data types.', list, [])
+		.option('--leaveTypes <leaveTypes>',
+			'Leave these data types. Takes precedence over removal.', list, [])
+		.option('--removeAll',
+			'Remove all data types, except for what is explicitly left.')
+		.option('--hashIDs',
+			'Pass IDs (such as _groupId and uploadId) through a one-way hash.')
+		.option('--removeSource',
+			'Remove the source of the data, e.g. carelink.')
+		.option('--removeTransmitter',
+			'Remove the transmitter id, e.g. the transmitter id for a Dexcom.')
+		.option('-v, --verbose',
+			'Verbose output.')
+		.parse(process.argv);
+
+	performDataStripping(function() {});
+
+	function performDataStripping(callback) {
+		checkOptions();
+		if (program.verbose) {
+			printOptions();
+		}
+
+		var ifs = makeInFileStream();
+
+		var jsonStream = JSONStream.parse('*');
+
+		var ofs = makeOutstream();
+
+		var first = true;
+		writeToOutstream(ofs, '[');
+
+		// Perform the parsing
+		ifs
+			.pipe(jsonStream)
+			.on('data', function (chunk) {
+
+				if ((program.removeAll
+					|| program.removeTypes
+					.indexOf(chunk.type) >= 0)
+					&& program.leaveTypes
+					.indexOf(chunk.type) < 0) {
+						// Do NOT add this event to output
+					return;
+				}
+
+				if (first)
+					first = false;
+				else
+					writeToOutstream(ofs, ',');
+
+				var cleanData = chunk;
+
+				stripBasalSuppressedInfo(cleanData);
+
+				removeAnnotations(cleanData);
+
+				removeIDsAndPayload(cleanData);
+
+				stripModelAndSNForData(cleanData);
+
+				hashIDsForData(cleanData);
+
+				removeSourceForData(cleanData);
+
+				removeTransmitterIdForData(cleanData);
+
+				writeToOutstream(ofs, JSON.stringify(cleanData));
+			})
+			.on('end', function() {
+				writeToOutstream(ofs, ']');
+				if (program.verbose) {
+					console.log(chalk.yellow.bold('Done writing to output.'));
+				}
+				callback();
+			});
 	}
-}
 
-function removeSourceForData(data) {
-	if (program.removeSource) {
-		delete data.source;
+
+	function checkOptions() {
+		if (!program.stripAll) {program.stripAll = false;}
+		if (!program.removeAll) {program.removeAll = false;}
+		if (!program.hashIDs) {program.hashIDs = false;}
+		if (!program.removeSource) {program.removeSource = false;}
+		if (!program.verbose) {program.verbose = false;}
 	}
-}
 
-function removeTransmitterIdForData(data) {
-	if (program.removeTransmitter) {
-		delete data.transmitterId;
+	function printOptions() {
+		console.log(chalk.blue.bold('input: ') + program.input);
+		console.log(chalk.blue.bold('output: ') + program.output);
+		console.log(chalk.blue.bold('stripModels: ') + program.stripModels);
+		console.log(chalk.blue.bold('stripSNs: ') + program.stripSNs);
+		console.log(chalk.blue.bold('leaveModels: ') + program.leaveModels);
+		console.log(chalk.blue.bold('leaveSNs: ') + program.leaveSNs);
+		console.log(chalk.blue.bold('stripAll: ') + program.stripAll);
+		console.log(chalk.blue.bold('removeTypes: ') + program.removeTypes);
+		console.log(chalk.blue.bold('leaveTypes: ') + program.leaveTypes);
+		console.log(chalk.blue.bold('removeAll: ') + program.removeAll);
+		console.log(chalk.blue.bold('hashIDs: ') + program.hashIDs);
+		console.log(chalk.blue.bold('removeSource: ') + program.removeSource);
+		console.log(chalk.blue.bold('verbose: ') + program.verbose);
 	}
-}
 
-function checkOptions() {
-	if (!program.stripAll) {program.stripAll = false;}
-	if (!program.removeAll) {program.removeAll = false;}
-	if (!program.hashIDs) {program.hashIDs = false;}
-	if (!program.removeSource) {program.removeSource = false;}
-	if (!program.verbose) {program.verbose = false;}
-}
-
-function printOptions() {
-	console.log(chalk.blue.bold('input: ') + program.input);
-	console.log(chalk.blue.bold('output: ') + program.output);
-	console.log(chalk.blue.bold('stripModels: ') + program.stripModels);
-	console.log(chalk.blue.bold('stripSNs: ') + program.stripSNs);
-	console.log(chalk.blue.bold('leaveModels: ') + program.leaveModels);
-	console.log(chalk.blue.bold('leaveSNs: ') + program.leaveSNs);
-	console.log(chalk.blue.bold('stripAll: ') + program.stripAll);
-	console.log(chalk.blue.bold('removeTypes: ') + program.removeTypes);
-	console.log(chalk.blue.bold('leaveTypes: ') + program.leaveTypes);
-	console.log(chalk.blue.bold('removeAll: ') + program.removeAll);
-	console.log(chalk.blue.bold('hashIDs: ') + program.hashIDs);
-	console.log(chalk.blue.bold('removeSource: ') + program.removeSource);
-	console.log(chalk.blue.bold('verbose: ') + program.verbose);
-}
-
-function makeInFileStream() {
-	var ifs;
-	if (program.input) {
-		ifs = fs.createReadStream(program.input, {encoding: 'utf8'});
-	} else {
-		ifs = process.stdin;
+	function makeInFileStream() {
+		var ifs;
+		if (program.input) {
+			ifs = fs.createReadStream(program.input, {encoding: 'utf8'});
+		} else {
+			ifs = process.stdin;
+		}
+		return ifs;
 	}
-	return ifs;
-}
 
-function makeOutstream() {
-	var ofs;
-	if (program.output) {
-		ofs = fs.createWriteStream(program.output);
-	} else {
-		ofs = process.stdout;
+	function makeOutstream() {
+		var ofs;
+		if (program.output) {
+			ofs = fs.createWriteStream(program.output);
+		} else {
+			ofs = process.stdout;
+		}
+		return ofs
 	}
-	return ofs
-}
 
-function writeToOutstream(ofs, info) {
-	if (program.output) {
-		ofs.write(info);
-	} else {
-		console.log(info);
+	function writeToOutstream(ofs, info) {
+		if (program.output) {
+			ofs.write(info);
+		} else {
+			console.log(info);
+		}
 	}
 }
